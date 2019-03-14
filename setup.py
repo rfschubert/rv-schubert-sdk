84a2ce5346bd2b378aa6fcc4274531e94d461498
Raphael Schubert <rfswdp@gmail.com>
2019-03-14 01:47:17 -0300

    updated version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -12,7 +12,7 @@ setup(
     author_email="rfswdp@gmail.com",
     description="SKD de integracao com RV Tecnologia",
     url="https://github.com/rfschubert/rv-schubert-sdk",
-    version='0.0.1',
+    version='0.0.2',
     packages=['rv_schubert_sdk'],
     long_description=readme,
     license=license,
